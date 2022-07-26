a500b868c482a056eedf3fa7404a559176eeecb1
lamine785260388 <lamine785260388@gmail.com>
2022-07-26 16:00:13 +0000

    stateless statefull

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +15,42 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(),
+      home: soir(),
+    );
+  }
+}
+
+class soir extends StatefulWidget {
+  const soir({Key? key}) : super(key: key);
+
+  @override
+  State<soir> createState() => _soirState();
+}
+
+class _soirState extends State<soir> {
+  var information = "Lamine Traore";
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    information = "bienvenue sur le tuto";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(information),
+      ),
+      body: RaisedButton(
+        child: Text('valider'),
+        onPressed: () {
+          print('validation ok');
+          setState(() {
+            information = "change title 2f";
+          });
+        },
+      ),
     );
   }
 }
